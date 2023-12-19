f01233caf252d27a05b62347d8472f74e7d57f3c
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-12-19 12:18:36 +0400

    updated solution for Problem 2, Day 1

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -4,38 +4,25 @@ var lines = new List<string?>();
 using var sr = new StreamReader("input.txt");
 while (sr.ReadLine() is { } line) lines.Add(line);
 
-var numbers = new List<int>();
-var index = 0;
-foreach (var line in lines)
-{
-    if (line == null) continue;
-    var matches = Regex.Matches(line, @"\d+");
-    var digits = new List<char>();
-    foreach (Match match in matches)
-    {
-        var num = int.Parse(match.Value);
-        digits.AddRange(num.ToString().ToCharArray());
-    }
+var sum = (from line in lines 
+    where line != null 
+    let firstMatch = Regex.Match(line, @"\d|one|two|three|four|five|six|seven|eight|nine") 
+    let lastMatch = Regex.Match(line, @"\d|one|two|three|four|five|six|seven|eight|nine", RegexOptions.RightToLeft) 
+    select ParseNum(firstMatch.Value) * 10 + ParseNum(lastMatch.Value)).Sum();
 
-    // if the number of digits in a line is less than 2, then duplicate the first digit
-    if (digits.Count < 2) digits.Add(digits[0]);
+Console.WriteLine(sum);
+return;
 
-    var numberInLine = $"{digits[0]}{digits[^1]}";
-    // Console.WriteLine($"{index}: Number: {numberInLine}");
-    try
-    {
-        numbers.Add(int.Parse(numberInLine));
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(
-            $"{index}: Tried the number {numberInLine} but it failed with the following exception: {ex.Message}");
-        throw;
-    }
+int ParseNum(string st) => st switch {
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+    var d => int.Parse(d)
+};
 
-    index++;
-}
-
-var sum = numbers.Sum();
-
-Console.WriteLine($"The output is: {sum}");
