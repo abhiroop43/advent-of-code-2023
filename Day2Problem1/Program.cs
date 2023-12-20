112b22ab8c47d99eba2f27355d5160434e3d993c
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-12-20 16:20:59 +0400

    bug fix and code refactor

--- Day2Problem1/Program.cs
+++ Day2Problem1/Program.cs
@@ -1,34 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
 
-using System.Text.RegularExpressions;
-
-var TOTAL_BLUE_BALLS = 14;
-var TOTAL_RED_BALLS = 12;
-var TOTAL_GREEN_BALLS = 13;
+const int totalBlueBalls = 14;
+const int totalRedBalls = 12;
+const int totalGreenBalls = 13;
 
 var lines = new List<string?>();
 using var sr = new StreamReader("input.txt");
 while (sr.ReadLine() is { } line) lines.Add(line);
 
-Dictionary<int, bool> gamePossibilities = new Dictionary<int, bool>();
-Dictionary<int, int> gamePowers = new Dictionary<int, int>();
+var gamePossibilities = new Dictionary<int, bool>();
+var gamePowers = new Dictionary<int, int>();
 
 foreach (var input in lines)
 {
-    string bluePattern = @"(\d+)\s*blue";
-    string redPattern = @"(\d+)\s*red";
-    string greenPattern = @"(\d+)\s*green";
+    const string bluePattern = @"(\d+)\s*blue";
+    const string redPattern = @"(\d+)\s*red";
+    const string greenPattern = @"(\d+)\s*green";
     
-    string gamePattern = @"Game (\d+):";
-    bool gamePossible = true;
+    const string gamePattern = @"Game (\d+):";
+    var gamePossible = true;
 
     if (input == null) continue;
-    MatchCollection blueMatches = Regex.Matches(input, bluePattern);
-    MatchCollection redMatches = Regex.Matches(input, redPattern);
-    MatchCollection greenMatches = Regex.Matches(input, greenPattern);
+    var blueMatches = Regex.Matches(input, bluePattern);
+    var redMatches = Regex.Matches(input, redPattern);
+    var greenMatches = Regex.Matches(input, greenPattern);
     
-    Match gameNumberMatch = Regex.Match(input, gamePattern);
-    int gameNumber = Convert.ToInt32(gameNumberMatch.Groups[1].Value);
+    var gameNumberMatch = Regex.Match(input, gamePattern);
+    var gameNumber = Convert.ToInt32(gameNumberMatch.Groups[1].Value);
 
     var blueBalls = new List<int>();
     foreach (Match match in blueMatches)
@@ -55,9 +53,9 @@ foreach (var input in lines)
     var maxGreenBalls = greenBalls.Max();
     var maxRedBalls = redBalls.Max();
     
-    if(maxBlueBalls > TOTAL_BLUE_BALLS 
-       || maxGreenBalls > TOTAL_RED_BALLS 
-       || maxRedBalls > TOTAL_GREEN_BALLS)
+    if(maxBlueBalls > totalBlueBalls 
+       || maxRedBalls > totalRedBalls 
+       || maxGreenBalls > totalGreenBalls)
     {
         gamePossible = false;
     }
@@ -65,6 +63,7 @@ foreach (var input in lines)
     gamePossibilities.Add(gameNumber, gamePossible);
     
     var gamePower = maxBlueBalls * maxGreenBalls * maxRedBalls;
+    
     // Console.WriteLine($"Key: {gameNumber}, Value: {gamePower}");
     gamePowers.Add(gameNumber, gamePower);
 }
@@ -75,15 +74,9 @@ var totalPossibleGames = gamePossibilities
     .Where(game => game.Value)
     .Sum(game => game.Key);
 
-int totalGamePower = 0;
-foreach (var game in gamePowers)
-{
-    if (gamePowers[game.Key] != 0)
-    {
-        totalGamePower += game.Value;
-    }
-}
-
+var totalGamePower = gamePowers
+    .Where(game => gamePowers[game.Key] != 0)
+    .Sum(game => game.Value);
 
 
 Console.WriteLine(totalPossibleGames);
